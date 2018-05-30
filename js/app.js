4c2bbd00a3e80f1aca9ffc75906d929d780931c1
yasuanke <ihshinron1@gmail.com>
2018-05-30 16:31:31 -0600

    Mejora

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ let arrayAlumno = [];
 
 if(localStorage.getItem('datos_alumnos') != null) {
   arrayAlumno = JSON.parse( localStorage.getItem('datos_alumnos') );
-  mostrarDatos();
 }
 
 function guardarRegistro() {
@@ -26,11 +25,8 @@ function guardarRegistro() {
 }
 
 function mostrarDatos() {
-  let tbody = document.querySelector('#tbAlumnoData tbody');
-
-  tbody.innerHTML = '';
-
-  let obtenerDatos = JSON.parse( localStorage.getItem('datos_alumnos') );
+  let tbody = document.querySelector('#tbAlumnoData tbody'),
+      obtenerDatos = JSON.parse( localStorage.getItem('datos_alumnos') );
 
   for(let i in obtenerDatos) {
     let array = obtenerDatos[i];
